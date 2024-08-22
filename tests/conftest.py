9edee292849d530107e0a1ad1836e869a182cc0e
vinimj18 <viniciusmaggiotto@gmail.com>
2024-08-22 19:45:17 +0200

    Add html report with screenshot functionality

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,10 @@
+# type: ignore
+
 import pytest
 
 from selenium import webdriver
+
+driver = None
 
 
 def pytest_addoption(parser):
@@ -11,6 +15,7 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope='class')
 def setup(request):
+    global driver
     browser_name = request.config.getoption('browser_name')
 
     if browser_name == 'chrome':
@@ -38,3 +43,30 @@ def setup(request):
     driver.get("https://rahulshettyacademy.com/angularpractice/")
 
     request.cls.driver = driver
+
+
+@pytest.mark.hookwrapper
+def pytest_runtest_makereport(item):
+    """
+        Extends the PyTest Plugin to take and embed screenshot in html report, whenever test fails.
+        :param item:
+        """
+    pytest_html = item.config.pluginmanager.getplugin('html')
+    outcome = yield
+    report = outcome.get_result()
+    extra = getattr(report, 'extra', [])
+
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            file_name = report.nodeid.replace("::", "_") + ".png"
+            _capture_screenshot(file_name)
+            if file_name:
+                html = '<div><img src="%s" alt="screenshot" style="width:304px;height:228px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % file_name
+                extra.append(pytest_html.extras.html(html))
+        report.extra = extra
+
+
+def _capture_screenshot(name):
+    driver.get_screenshot_as_file(name)
